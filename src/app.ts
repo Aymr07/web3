5d00d2558dba780464688ae5791406ac24f45fb4
Yousuf <yousuf5550@gmail.com>
2021-06-20 12:53:05 +0400

    fuck

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,12 @@
-import web3 from "web3";
+import Web3 from "web3";
+
+(async () => {
+    const url : string = "https://bsc-dataseed1.binance.org";
+    const web3 = new Web3(url);
+    web3.eth.getBlockNumber()
+    .then(blockNumber => console.log(blockNumber))
+    .catch(console.error)
+})();
+
+
 
